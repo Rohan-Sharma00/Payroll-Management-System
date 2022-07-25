dcdc2562b2161d91ea9f4cf462ec1017a979e2d6
Bhavesh Khandelwal <khandelwal.bhavesh0@gmail.com>
2022-07-25 23:35:34 +0530

    Added calender and time widgets

--- pay/models.py
+++ pay/models.py
@@ -14,9 +14,9 @@ class Employee(models.Model):
     
 class Attendance(models.Model):
     employee = models.ForeignKey("Employee", related_name="attendance_employee", on_delete=models.CASCADE,)
-    date = models.CharField(max_length=10)
-    loginTime = models.CharField(max_length=10)
-    logoutTime = models.CharField(max_length=10)
+    date = models.DateField(null=True)
+    loginTime = models.TimeField(null=True)
+    logoutTime = models.TimeField(null=True)
     attendance_create_time = models.DateTimeField(auto_now=True)
 
 
